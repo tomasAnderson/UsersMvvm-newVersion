335a731603a1ed6f45a25f685f3fc37e1643f30c
Туров Андрей <andrey.turoff@mail.ru>
2020-10-01 12:00:49 +0300

    final change

--- UsersMvvm/Converters/ParamsConverter.cs
+++ UsersMvvm/Converters/ParamsConverter.cs
@@ -13,7 +13,7 @@ namespace UsersMvvm.Converters
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
             return values.ToList();
-        }
+        } 
         
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
